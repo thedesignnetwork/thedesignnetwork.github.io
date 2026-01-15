52555c01b68535fbc4a496014f13cbcd9f2dfe81
ROHAN PANDEY <95585299+rohan-pandeyy@users.noreply.github.com>
2026-01-15 21:38:11 +0530

    Setup Sanity Backend and Update Workflow (#7)
    * feat: setup sanity backend and github pages deployment
    
    * feat:  pages deployment workflow

--- backend/sanity.cli.ts
+++ backend/sanity.cli.ts
@@ -3,13 +3,14 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'l40bck5h',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
+    appId: 'j5saau54unpne86fjjt99sj8',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
     autoUpdates: true,
-  }
+  },
 })
